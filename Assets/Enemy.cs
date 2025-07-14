cdca5bf0395d70498386a43c052422504bc30c5e
MFuentesSAE <208415671+MFuentesSAE@users.noreply.github.com>
2025-07-14 09:48:27 -0600

    Creación de enemigo

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Greetings();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void Greetings()
+    {
+        Debug.Log("Waza");
+    }
+}
